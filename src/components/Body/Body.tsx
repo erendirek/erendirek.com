b753956e778e2cef02a48964e40171cd87f38ab8
Eren Direk <erendirek2145@gmail.com>
2025-08-19 17:39:26 +0300

    feat(layout): Create Body and Footer components for main page structure

--- src/components/Body/Body.tsx
+++ src/components/Body/Body.tsx
@@ -1,12 +1,18 @@
 import React from "react";
 import Home from "./Home/Home";
 import About from "./About/About";
+import Projects from "./Projects/Projects";
+import Blogs from "./Blogs/Blogs";
+import Contact from "./Contact/Contact";
 
 export default function Body() {
     return (
         <main className="min-h-screen">
             <Home />
             <About />
+            <Projects />
+            <Blogs />
+            <Contact />
         </main>
     );
 }
---
+++ src/components/Footer/Footer.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+export default function Footer() {
+    return (
+        <div className="flex h-32 snap-start items-center justify-center">
+            <p className="text-palette-red red-text-glow">by eren direk</p>
+        </div>
+    );
+}
