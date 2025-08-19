820b85e6d82539841730a924f38fbdcebef108c7
Eren Direk <erendirek2145@gmail.com>
2025-08-19 17:39:27 +0300

    feat(contact): Add Contact section with social media links and form

---
+++ src/components/Body/Contact/Contact.tsx
@@ -0,0 +1,143 @@
+import TypeWriterText from "@/components/TypeWriterText/TypeWriterText";
+import { share_tech_mono } from "@/ui/fonts";
+import clsx from "clsx";
+import React, { ReactNode } from "react";
+import { LuLinkedin, LuGithub, LuLink, LuMail } from "react-icons/lu";
+import { IconType } from "react-icons";
+
+function ContactLink({
+    iconType,
+    text,
+    link,
+}: {
+    iconType: IconType;
+    text: string;
+    link: string;
+}) {
+    return (
+        <div className="flex">
+            <a href={link} target="_blank">
+                <div className="group flex items-center gap-x-3">
+                    {(() => {
+                        return React.createElement(iconType, {
+                            className:
+                                "text-palette-white group-hover:group-hover-white-svg-shadow",
+                            size: 32,
+                        });
+                    })()}
+                    <p className="text-palette-white group-hover:group-hover-white-text-shadow text-2xl">
+                        {text}
+                    </p>
+                </div>
+            </a>
+        </div>
+    );
+}
+export default function Contact() {
+    return (
+        <div id="contact" className="section relative h-screen snap-start">
+            <div className="fullscreen-green-glow" />
+            <div className="container mx-auto flex h-full flex-col pt-32">
+                <div className="flex">
+                    <p
+                        className={clsx(
+                            `${share_tech_mono.className} antialiased}`,
+                            "text-palette-green text-7xl",
+                            "header-green-glow-animation",
+                        )}
+                    >
+                        [eren ~]$&#x2800;
+                    </p>
+                    <TypeWriterText
+                        text="cat contact"
+                        animationDuration={600}
+                        cursor
+                        reanimateOnScroll
+                        className={clsx(
+                            `${share_tech_mono.className} antialiased}`,
+                            "text-palette-green text-7xl",
+                            "header-green-glow-animation",
+                        )}
+                    />
+                </div>
+                <div className="mt-24 flex h-full flex-1 items-stretch px-4">
+                    <div className="flex-1">
+                        <div className="flex flex-col gap-y-8">
+                            <ContactLink
+                                iconType={LuLinkedin}
+                                text="erndrk"
+                                link="https://www.linkedin.com/in/erndrk/"
+                            />
+                            <ContactLink
+                                iconType={LuGithub}
+                                text="erendirek"
+                                link="https://github.com/erendirek"
+                            />
+                            {/* <ContactLink
+                                iconType={LuLink}
+                                text="erendirek.com"
+                                link="https://erendirek.com"
+                            /> */}
+                            <ContactLink
+                                iconType={LuMail}
+                                text="erendirek2145@gmail.com"
+                                link="mailto:erendirek2145@gmail.com"
+                            />
+                        </div>
+                    </div>
+                    <div className="flex-1">
+                        <form className="w-full">
+                            <div className="flex flex-col gap-y-2">
+                                <input
+                                    type="email"
+                                    name="email"
+                                    id="email"
+                                    className="bg-palette-black border-palette-white text-palette-white peer order-1 border-2 px-2 py-1 outline-none"
+                                />
+                                <label
+                                    htmlFor="email"
+                                    className="text-palette-white peer-focus:group-hover-white-text-shadow text-xl transition-all"
+                                >
+                                    Email:
+                                </label>
+                            </div>
+                            <div className="mt-8 flex flex-col gap-y-2">
+                                <input
+                                    type="text"
+                                    name="title"
+                                    id="title"
+                                    className="bg-palette-black border-palette-white text-palette-white peer order-1 border-2 px-2 py-1 outline-none"
+                                />
+                                <label
+                                    htmlFor="title"
+                                    className="text-palette-white peer-focus:group-hover-white-text-shadow text-xl transition-all"
+                                >
+                                    Subject:
+                                </label>
+                            </div>
+                            <div className="mt-8 flex flex-col gap-y-2">
+                                <textarea
+                                    name="title"
+                                    id="title"
+                                    rows={4}
+                                    className="bg-palette-black border-palette-white text-palette-white peer order-1 border-2 px-2 py-1 outline-none"
+                                />
+                                <label
+                                    htmlFor="title"
+                                    className="text-palette-white peer-focus:group-hover-white-text-shadow text-xl transition-all"
+                                >
+                                    Message:
+                                </label>
+                            </div>
+                            <div className="mt-8">
+                                <button className="text-palette-black border-palette-yellow bg-palette-yellow hover:bg-palette-black hover:text-palette-yellow w-full border-2 py-4 text-xl font-bold">
+                                    Submit
+                                </button>
+                            </div>
+                        </form>
+                    </div>
+                </div>
+            </div>
+        </div>
+    );
+}
