78e5e5973674bb8a4239f8ecd6aef9f003dd49b4
Eren Direk <erendirek2145@gmail.com>
2025-08-19 17:44:23 +0300

    feat(projects): Add Projects section component

---
+++ src/components/Body/Projects/Projects.tsx
@@ -0,0 +1,55 @@
+import { share_tech_mono } from "@/ui/fonts";
+import clsx from "clsx";
+import React from "react";
+import TypeWriterText from "@/components/TypeWriterText/TypeWriterText";
+import SlideShow from "@/components/SlideShow/SlideShow";
+import SlideShowElement from "@/components/SlideShow/SlideShowElement";
+
+export default function Projects() {
+    return (
+        <div id="projects" className="section h-screen snap-start">
+            <div className="container mx-auto h-full pt-32">
+                <div className="flex">
+                    <p
+                        className={clsx(
+                            `${share_tech_mono.className} antialiased}`,
+                            "text-palette-green text-7xl",
+                            "header-green-glow-animation",
+                        )}
+                    >
+                        [eren ~]$&#x2800;
+                    </p>
+                    <TypeWriterText
+                        text="cat projects"
+                        animationDuration={600}
+                        cursor
+                        reanimateOnScroll
+                        className={clsx(
+                            `${share_tech_mono.className} antialiased}`,
+                            "text-palette-green text-7xl",
+                            "header-green-glow-animation",
+                        )}
+                    />
+                </div>
+                <div className="mt-32 flex h-96 items-stretch px-4">
+                    <SlideShow className="flex-1" shownContentCount={2}>
+                        <SlideShowElement
+                            title="Yaten's Horror Session"
+                            description="My first game published on Google Play."
+                            imageURL="https://play-lh.googleusercontent.com/DEFNnl683Z9a-th7uJb8cx6h7ntIetyxItNlxkIP-m6t4noOsUqZG2yAUJohac98Zrg=w416-h235-rw"
+                            imageAlt="yhs-1-image"
+                            link="https://play.google.com/store/apps/details?id=com.YEDgames.YatensHorrorSession&hl=tr"
+                        />
+                        <SlideShowElement
+                            title="Yaten's Horror Session II"
+                            description="My second game published on Google Play."
+                            imageURL="https://play-lh.googleusercontent.com/ttZ_Ys0kP8yEK6QiRVpSDFSpJeOrCAA867HKAwL4REmrOcdM6e7x3Q8c57A-0b3kORs=w416-h235-rw"
+                            imageAlt="yhs-2-image"
+                            link="https://play.google.com/store/apps/details?id=com.YEDgames.yatenshorrorsessionii&hl=tr"
+                        />
+                    </SlideShow>
+                </div>
+            </div>
+        </div>
+    );
+}
