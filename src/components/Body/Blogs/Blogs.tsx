8933631500bd56e77f78c87d2010a9eb98c2010b
Eren Direk <erendirek2145@gmail.com>
2025-08-20 15:26:20 +0300

    blogs page responsive updated

--- src/components/Body/Blogs/Blogs.tsx
+++ src/components/Body/Blogs/Blogs.tsx
@@ -8,12 +8,14 @@ export default function Blogs() {
     return (
         <div id="blogs" className="section h-screen snap-start">
             <div className="container mx-auto flex h-full flex-col pt-32">
-                <div className="flex">
+                <div className="flex pt-4">
                     <p
                         className={clsx(
                             `${share_tech_mono.className} antialiased}`,
-                            "text-palette-green text-7xl",
+                            "text-center text-6xl",
+                            "text-palette-green lg:text-7xl",
                             "header-green-glow-animation",
+                            "hidden md:block",
                         )}
                     >
                         [eren ~]$&#x2800;
@@ -25,15 +27,17 @@ export default function Blogs() {
                         reanimateOnScroll
                         className={clsx(
                             `${share_tech_mono.className} antialiased}`,
-                            "text-palette-green text-7xl",
+                            "text-center text-5xl md:text-6xl",
+                            "text-palette-green lg:text-7xl",
                             "header-green-glow-animation",
+                            "flex-1 text-center md:text-left",
                         )}
                     />
                 </div>
                 <div className="mt-12 flex flex-1 items-stretch px-4">
                     <p
                         className={clsx(
-                            "text-palette-red text-5xl",
+                            "text-palette-red text-center text-3xl md:text-left md:text-4xl lg:text-5xl",
                             "red-text-glow",
                         )}
                     >
